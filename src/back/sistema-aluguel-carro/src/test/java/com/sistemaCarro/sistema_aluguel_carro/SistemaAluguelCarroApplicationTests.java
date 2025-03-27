9259c176ba5dec4d6ed9cd9dffade8908d63abb9
gustavo <129975733+gustavocomam@users.noreply.github.com>
2025-03-26 22:10:26 -0300

    Add files via upload

---
+++ src/back/sistema-aluguel-carro/src/test/java/com/sistemaCarro/sistema_aluguel_carro/SistemaAluguelCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaCarro.sistema_aluguel_carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAluguelCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
