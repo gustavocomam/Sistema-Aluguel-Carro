661d7d0871000248e406d79a0bc34405c96f8d91
gustavo <129975733+gustavocomam@users.noreply.github.com>
2025-03-26 22:12:20 -0300

    Add files via upload

---
+++ src/back/sistema-aluguel-carro/src/main/java/com/sistemaCarro/sistema_aluguel_carro/Model/Usuario.java
@@ -0,0 +1,9 @@
+package com.sistemaCarro.sistema_aluguel_carro.Model;
+
+public class Usuario {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
---
+++ src/back/sistema-aluguel-carro/src/main/java/com/sistemaCarro/sistema_aluguel_carro/SistemaAluguelCarroApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaCarro.sistema_aluguel_carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAluguelCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAluguelCarroApplication.class, args);
+	}
+
+}
