5139beddd55f41f0b6fd1c42e1795ce4ac43432b
LucasPorto18 <124475594+LucasPorto18@users.noreply.github.com>
2025-03-27 01:12:28 -0300

    Delete src/back directory

--- src/back/sistema-aluguel-carro/src/main/java/com/sistemaCarro/sistema_aluguel_carro/Model/Usuario.java
+++
@@ -1,9 +0,0 @@
-package com.sistemaCarro.sistema_aluguel_carro.Model;
-
-public class Usuario {
-
-    private long id;
-    private String nome;
-    private String email;
-    private String senha;
-}
