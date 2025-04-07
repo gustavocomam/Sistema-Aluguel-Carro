f8ff4888a1818b558c5828ae93178f89e8eecdcd
gustavo <129975733+gustavocomam@users.noreply.github.com>
2025-04-07 00:27:25 -0300

    Add files via upload

---
+++ src/aluguel-carros/src/main/java/com/example/aluguel_carros/controller/LoginController.java
@@ -0,0 +1,69 @@
+package com.example.aluguel_carros.controller;
+
+import com.example.aluguel_carros.model.UsuarioModel;
+import com.example.aluguel_carros.service.CadastroService;
+import com.example.aluguel_carros.service.LoginService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+
+
+@Controller
+@RequestMapping("/usuario")
+public class LoginController {
+
+    private final LoginService loginService;
+    private final CadastroService cadastroService;
+
+    public LoginController(LoginService loginService, CadastroService cadastroService) {
+        this.loginService = loginService;
+        this.cadastroService = cadastroService;
+    }
+
+    // Página de login
+    @GetMapping("/login")
+    public String mostrarLogin(@RequestParam(required = false) String erro, Model model) {
+        model.addAttribute("usuario", new UsuarioModel());
+        model.addAttribute("erro", erro != null);
+        return "Login";
+    }
+
+    // Processar login
+    @PostMapping("/login")
+    public String login(@ModelAttribute UsuarioModel usuarioForm) {
+        UsuarioModel usuario = loginService.autenticar(usuarioForm.getEmail(), usuarioForm.getSenha());
+
+        if (usuario == null) {
+            return "redirect:/usuario/login?erro=true";
+        }
+
+        if (usuario.getTipo() == UsuarioModel.TipoUsuario.AGENTE) {
+            return "redirect:/adm"; // ADM
+        } else {
+            return "redirect:/cliente"; // Cliente
+        }
+    }
+    // Página de cadastro
+    @GetMapping("/cadastro")
+    public String mostrarCadastro(Model model) {
+        model.addAttribute("usuario", new UsuarioModel()); // necessário pro th:object
+        return "Cadastro";
+    }
+
+    // Processar cadastro
+    @PostMapping("/cadastro")
+    public String cadastrar(@ModelAttribute UsuarioModel usuario) {
+        cadastroService.cadastrarUsuario(usuario);
+        return "redirect:/usuario/login";
+    }
+    @GetMapping("/logout")
+    public String sair() {
+        return "redirect:/";
+    }
+    @GetMapping("/reserva")
+    public String mostrarReserva() {
+        return "Reserva";
+    }
+}
+
